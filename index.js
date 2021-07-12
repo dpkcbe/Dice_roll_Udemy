807ef8b734adfcae160e3448f40fc15c49819eab
Deepak <dpkcbe@gmail.com>
2021-07-12 23:04:34 +0530

    Dice commit2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var a = Math.ceil(Math.random() * 6);
-var randomImage = "Images/dice" + a + ".png";
+var randomImage1 = "Images/dice" + a + ".png";
 var image = document.querySelectorAll("img")[0];
-image.setAttribute("src", randomImage);
+image.setAttribute("src", randomImage1);
 
 var b = Math.ceil(Math.random() * 6);
-var randommage = "Images/dice" + b + ".png";
+var randomImage2 = "Images/dice" + b + ".png";
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randommage);
+image2.setAttribute("src", randomImage2);
 
 if (a > b) {
     document.querySelector("h1").innerHTML = "Yayy! Player1 Won!";
